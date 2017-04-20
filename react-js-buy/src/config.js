cebdfdfbc451a55413144cd06c718c95d4e37900
Leanne Shapton <leanne.shapton@shopify.com>
2017-04-20 09:42:22 -0400

    removed config.js

--- react-js-buy/src/config.js
+++
@@ -1,8 +0,0 @@
-import Client, {Config} from 'shopify-buy';
-
-const config = new Config({
-  storefrontAccessToken: 'dd4d4dc146542ba7763305d71d1b3d38',
-  domain: 'graphql.myshopify.com',
-});
-
-export const client = new Client(config);

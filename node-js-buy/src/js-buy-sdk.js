d7e9b89b208eae66fba79ef48502ae365073eda2
Faheem Nusrat <faheem.nusrat@shopify.com>
2019-05-16 16:37:45 +0100

    Revert commit
    storefrontAccessToken: 'dd4d4dc146542ba7763305d71d1b3d38',
      domain: 'graphql.myshopify.com'
    });

--- node-js-buy/src/js-buy-sdk.js
+++ node-js-buy/src/js-buy-sdk.js
@@ -4,8 +4,8 @@ import Client from 'shopify-buy';
 global.fetch = fetch;
 
 const client = Client.buildClient({
-  storefrontAccessToken: '20ef0127c6c37cee73dd149879a379a8',
-  domain: 'demofnstore.myshopify.com'
+	storefrontAccessToken: 'dd4d4dc146542ba7763305d71d1b3d38',
+  	domain: 'graphql.myshopify.com'
 });
 
 export default client;

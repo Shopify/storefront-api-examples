c703e765a5ae6c712f326bacb30c8602bd600e47
Jarrod Flesch <jarrodmflesch@gmail.com>
2020-02-02 17:50:42 -0500

    adds eslint, editor config for sanity -- also updates the package.json
    adds the provider/context code that allows us to abstract out the functions from the UI
    
    adds the updated components that are using the hoc wrapper to access and manipulate the store
    
    reorganizes the folder structure for clarity, refactors LineItem as functional component
    
    correctly sets the authErrors with a function
    
    fixes generic authError not being set properly

--- react-apollo/src/components/Cart.js
+++ react-apollo/src/components/Cart.js
@@ -1,66 +1,79 @@
-import React, {Component} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import LineItem from './LineItem';
 
-class Cart extends Component {
-  constructor(props) {
-  super(props);
+import withStoreContext from '../withStoreContext';
 
-    this.openCheckout = this.openCheckout.bind(this);
-  }
+const Cart  = (props) => {
+  const {
+    storeContext: {
+      checkout,
+      redirectToWebCheckout,
+      isCartOpen,
+      handleCartClose,
+    }
+  } = props;
 
-  openCheckout() {
-    window.open(this.props.checkout.webUrl);
-  }
+  return (
+    <div className={`Cart ${isCartOpen ? 'Cart--open' : ''}`}>
+				<header className="Cart__header">
+					<h2>Cart</h2>
+					<button
+						onClick={handleCartClose}
+						className="Cart__close"
+					>
+						×
+					</button>
+				</header>
+				<ul className="Cart__line-items">
+          {(checkout && checkout.lineItems.edges && checkout.lineItems.edges.length > 0) && checkout.lineItems.edges.map(lineItem => {
+            return (
+              <LineItem
+                key={lineItem.node.id.toString()}
+                line_item={lineItem.node}
+              />
+            );
+          })}
+				</ul>
 
-  render() {
-    let line_items = this.props.checkout.lineItems.edges.map((line_item) => {
-      return (
-        <LineItem
-          removeLineItemInCart={this.props.removeLineItemInCart}
-          updateLineItemInCart={this.props.updateLineItemInCart}
-          key={line_item.node.id.toString()}
-          line_item={line_item.node}
-        />
-      );
-    });
+				<footer className="Cart__footer">
+					<div className="Cart-info clearfix">
+						<div className="Cart-info__total Cart-info__small">Subtotal</div>
+						<div className="Cart-info__pricing">
+							<span className="pricing">$ {checkout && checkout.subtotalPrice}</span>
+						</div>
+					</div>
+					<div className="Cart-info clearfix">
+						<div className="Cart-info__total Cart-info__small">Taxes</div>
+						<div className="Cart-info__pricing">
+							<span className="pricing">$ {checkout && checkout.totalTax}</span>
+						</div>
+					</div>
+					<div className="Cart-info clearfix">
+						<div className="Cart-info__total Cart-info__small">Total</div>
+						<div className="Cart-info__pricing">
+							<span className="pricing">$ {checkout && checkout.totalPrice}</span>
+						</div>
+					</div>
+					<button
+						className="Cart__checkout button"
+            onClick={redirectToWebCheckout}
+            type="button"
+					>
+            Checkout
+					</button>
+				</footer>
+			</div>
+  )
+};
 
-    return (
-      <div className={`Cart ${this.props.isCartOpen ? 'Cart--open' : ''}`}>
-        <header className="Cart__header">
-          <h2>Cart</h2>
-          <button
-            onClick={this.props.handleCartClose}
-            className="Cart__close">
-            ×
-          </button>
-        </header>
-        <ul className="Cart__line-items">
-          {line_items}
-        </ul>
-        <footer className="Cart__footer">
-          <div className="Cart-info clearfix">
-            <div className="Cart-info__total Cart-info__small">Subtotal</div>
-            <div className="Cart-info__pricing">
-              <span className="pricing">$ {this.props.checkout.subtotalPrice}</span>
-            </div>
-          </div>
-          <div className="Cart-info clearfix">
-            <div className="Cart-info__total Cart-info__small">Taxes</div>
-            <div className="Cart-info__pricing">
-              <span className="pricing">$ {this.props.checkout.totalTax}</span>
-            </div>
-          </div>
-          <div className="Cart-info clearfix">
-            <div className="Cart-info__total Cart-info__small">Total</div>
-            <div className="Cart-info__pricing">
-              <span className="pricing">$ {this.props.checkout.totalPrice}</span>
-            </div>
-          </div>
-          <button className="Cart__checkout button" onClick={this.openCheckout}>Checkout</button>
-        </footer>
-      </div>
-    )
-  }
-}
+Cart.propTypes = {
+	storeContext: PropTypes.shape({
+    checkout: PropTypes.shape({}),
+    isCartOpen: PropTypes.bool,
+    handleCartClose: PropTypes.func,
+    redirectToWebCheckout: PropTypes.string,
+	}).isRequired,
+};
 
-export default Cart;
+export default withStoreContext(Cart);

d07b46c6b68afb7f14e4c1402e93ea5113c9beec
Neil Mispelaar <neil.mispelaar@outlook.com>
2020-06-29 17:19:10 -0400

    Refactor to make code more readable

--- vue-graphql-client/src/main.ts
+++ vue-graphql-client/src/main.ts
@@ -2,4 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import store from './store';
 
-createApp(App).use(store).mount('#app');
+// Setup Vue App and the add-ons
+const app = createApp(App);
+app.use(store);
+
+app.mount('#app');

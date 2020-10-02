560c06513d70c04980b751d84f16119faeef4f0f
LishaCoder <58967040+lisha-rajput@users.noreply.github.com>
2020-10-02 12:37:16 +0530

    Removed unwanted code
    remove some unwanted code

--- react-graphql-client/scripts/build.js
+++ react-graphql-client/scripts/build.js
@@ -72,13 +72,9 @@ function build(previousFileSizes) {
    }
 
     console.log(chalk.green('Compiled successfully.'));
-    console.log();
-
     console.log('File sizes after gzip:');
-    console.log();
+   
     printFileSizesAfterBuild(stats, previousFileSizes);
-    console.log();
-
     var appPackage  = require(paths.appPackageJson);
     var publicUrl = paths.publicUrl;
     var publicPath = config.output.publicPath;
@@ -87,12 +83,10 @@ function build(previousFileSizes) {
       // "homepage": "http://user.github.io/project"
       console.log('The project was built assuming it is hosted at ' + chalk.green(publicPathname) + '.');
       console.log('You can control this with the ' + chalk.green('homepage') + ' field in your '  + chalk.cyan('package.json') + '.');
-      console.log();
       console.log('The ' + chalk.cyan('build') + ' folder is ready to be deployed.');
       console.log('To publish it at ' + chalk.green(publicUrl) + ', run:');
       // If script deploy has been added to package.json, skip the instructions
       if (typeof appPackage.scripts.deploy === 'undefined') {
-        console.log();
         if (useYarn) {
           console.log('  ' + chalk.cyan('yarn') +  ' add --dev gh-pages');
         } else {
@@ -100,53 +94,48 @@ function build(previousFileSizes) {
         }
         console.log();
         console.log('Add the following script in your ' + chalk.cyan('package.json') + '.');
-        console.log();
+        
         console.log('    ' + chalk.dim('// ...'));
         console.log('    ' + chalk.yellow('"scripts"') + ': {');
         console.log('      ' + chalk.dim('// ...'));
         console.log('      ' + chalk.yellow('"predeploy"') + ': ' + chalk.yellow('"npm run build",'));
         console.log('      ' + chalk.yellow('"deploy"') + ': ' + chalk.yellow('"gh-pages -d build"'));
         console.log('    }');
-        console.log();
+        
         console.log('Then run:');
       }
-      console.log();
+      
       console.log('  ' + chalk.cyan(useYarn ? 'yarn' : 'npm') +  ' run deploy');
-      console.log();
+     
     } else if (publicPath !== '/') {
       // "homepage": "http://mywebsite.com/project"
       console.log('The project was built assuming it is hosted at ' + chalk.green(publicPath) + '.');
       console.log('You can control this with the ' + chalk.green('homepage') + ' field in your '  + chalk.cyan('package.json') + '.');
-      console.log();
       console.log('The ' + chalk.cyan('build') + ' folder is ready to be deployed.');
-      console.log();
+     
     } else {
       if (publicUrl) {
         // "homepage": "http://mywebsite.com"
         console.log('The project was built assuming it is hosted at ' + chalk.green(publicUrl) +  '.');
         console.log('You can control this with the ' + chalk.green('homepage') + ' field in your '  + chalk.cyan('package.json') + '.');
-        console.log();
       } else {
         // no homepage
         console.log('The project was built assuming it is hosted at the server root.');
         console.log('To override this, specify the ' + chalk.green('homepage') + ' in your '  + chalk.cyan('package.json') + '.');
         console.log('For example, add this to build it for GitHub Pages:')
-        console.log();
         console.log('  ' + chalk.green('"homepage"') + chalk.cyan(': ') + chalk.green('"http://myname.github.io/myapp"') + chalk.cyan(','));
-        console.log();
       }
       var build = path.relative(process.cwd(), paths.appBuild);
       console.log('The ' + chalk.cyan(build) + ' folder is ready to be deployed.');
       console.log('You may serve it with a static server:');
-      console.log();
+      
       if (useYarn) {
         console.log(`  ${chalk.cyan('yarn')} global add serve`);
       } else {
         console.log(`  ${chalk.cyan('npm')} install -g serve`);
       }
       console.log(`  ${chalk.cyan('serve')} -s build`);
-      console.log();
-    }
+   }
   });
 }
 
